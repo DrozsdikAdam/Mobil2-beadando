e75c6719d2a271818f6b3858a29a90e03cab95fe
DrozsdikAdam <drozsdik17@gmail.com>
2026-03-27 19:37:26 +0100

    out sourceing the hardcoded texts and colors into the strings.xml and colors.xml files

--- RealTimeChatApplication/app/src/main/java/com/example/realtimechatapplication/MainActivity.java
+++ RealTimeChatApplication/app/src/main/java/com/example/realtimechatapplication/MainActivity.java
@@ -24,11 +24,11 @@ public class MainActivity extends AppCompatActivity {
 
         // Adatlista létrehozása és feltöltése tesztadatokkal
         chatList = new ArrayList<>();
-        chatList.add(new ChatModel("Teszt Elek", "Szia! Mi újság?", "12:00", R.drawable.ic_person));
-        chatList.add(new ChatModel("Kovács Anna", "Mikor találkozunk?", "11:45", R.drawable.ic_person));
-        chatList.add(new ChatModel("Nagy Gábor", "Elküldtem a fájlt.", "09:30", R.drawable.ic_person));
-        chatList.add(new ChatModel("Szabó Bence", "Köszi a segítséget!", "Tegnap", R.drawable.ic_person));
-        chatList.add(new ChatModel("Horváth Dóra", "Szia, mizu?", "Tegnap", R.drawable.ic_person));
+        chatList.add(new ChatModel(getString(R.string.user_name_1), getString(R.string.message_1), getString(R.string.time_1), R.drawable.ic_person));
+        chatList.add(new ChatModel(getString(R.string.user_name_2), getString(R.string.message_2), getString(R.string.time_2), R.drawable.ic_person));
+        chatList.add(new ChatModel(getString(R.string.user_name_3), getString(R.string.message_3), getString(R.string.time_3), R.drawable.ic_person));
+        chatList.add(new ChatModel(getString(R.string.user_name_4), getString(R.string.message_4), getString(R.string.time_yesterday), R.drawable.ic_person));
+        chatList.add(new ChatModel(getString(R.string.user_name_5), getString(R.string.message_5), getString(R.string.time_yesterday), R.drawable.ic_person));
 
         // Adapter beállítása
         chatAdapter = new ChatAdapter(chatList);
