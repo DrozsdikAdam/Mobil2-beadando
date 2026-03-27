648648a329777aeff736ded8c42dcb0375b84021
DrozsdikAdam <drozsdik17@gmail.com>
2026-03-27 19:21:07 +0100

    connecting the adapter to the view and creating test data

--- RealTimeChatApplication/app/src/main/java/com/example/realtimechatapplication/MainActivity.java
+++ RealTimeChatApplication/app/src/main/java/com/example/realtimechatapplication/MainActivity.java
@@ -1,24 +1,37 @@
 package com.example.realtimechatapplication;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ChatAdapter chatAdapter;
+    private List<ChatModel> chatList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // RecyclerView inicializálása
+        recyclerView = findViewById(R.id.chatList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Adatlista létrehozása és feltöltése tesztadatokkal
+        chatList = new ArrayList<>();
+        chatList.add(new ChatModel("Teszt Elek", "Szia! Mi újság?", "12:00", R.drawable.ic_person));
+        chatList.add(new ChatModel("Kovács Anna", "Mikor találkozunk?", "11:45", R.drawable.ic_person));
+        chatList.add(new ChatModel("Nagy Gábor", "Elküldtem a fájlt.", "09:30", R.drawable.ic_person));
+        chatList.add(new ChatModel("Szabó Bence", "Köszi a segítséget!", "Tegnap", R.drawable.ic_person));
+        chatList.add(new ChatModel("Horváth Dóra", "Szia, mizu?", "Tegnap", R.drawable.ic_person));
+
+        // Adapter beállítása
+        chatAdapter = new ChatAdapter(chatList);
+        recyclerView.setAdapter(chatAdapter);
     }
 }
