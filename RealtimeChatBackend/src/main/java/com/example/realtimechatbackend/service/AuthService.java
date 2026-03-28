915e67e7fdfe06b0391252e5ade7e9213662c8c3
DrozsdikAdam <drozsdik17@gmail.com>
2026-03-28 19:15:12 +0100

    Adding the login method

--- RealtimeChatBackend/src/main/java/com/example/realtimechatbackend/service/AuthService.java
+++ RealtimeChatBackend/src/main/java/com/example/realtimechatbackend/service/AuthService.java
@@ -1,11 +1,18 @@
 package com.example.realtimechatbackend.service;
 
+import com.example.realtimechatbackend.dto.LoginRequestDto;
 import com.example.realtimechatbackend.dto.RegisterRequestDto;
+import com.example.realtimechatbackend.exception.InvalidCredentialsException;
+import com.example.realtimechatbackend.exception.InvalidPasswordFormatException;
+import com.example.realtimechatbackend.exception.UserAlreadyExistsException;
+import com.example.realtimechatbackend.exception.UserNotFoundException;
 import com.example.realtimechatbackend.model.User;
 import com.example.realtimechatbackend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,17 +20,17 @@ public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public void register(RegisterRequestDto registerRequest) throws Exception {
+    public void register(RegisterRequestDto registerRequest) {
         if (userRepository.existsByUsernameAndIsDeletedFalse(registerRequest.getUsername())) {
-            throw new Exception("Username already exists!");
+            throw new UserAlreadyExistsException("Username already exists!");
         }
 
         if (userRepository.existsByEmailAndIsDeletedFalse(registerRequest.getEmail())) {
-            throw new Exception("Email already exists!");
+            throw new UserAlreadyExistsException("Email already exists!");
         }
 
         if (!registerRequest.getPassword().matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$")) {
-            throw new Exception("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character!");
+            throw new InvalidPasswordFormatException("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character!");
         }
 
         String encodedPassword = passwordEncoder.encode(registerRequest.getPassword());
@@ -35,6 +42,24 @@ public class AuthService {
         user.setPassword(encodedPassword);
         user.setIsDeleted(false);
         user.setIsOnline(false);
+
+        userRepository.save(user);
+    }
+
+    public void login(LoginRequestDto loginRequest) {
+        Optional<User> optionalUser = userRepository.findByEmailAndIsDeletedFalse(loginRequest.getEmail());
+
+        if (optionalUser.isEmpty()) {
+            throw new UserNotFoundException("User not found");
+        }
+
+        User user = optionalUser.get();
+
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            throw new InvalidCredentialsException("Invalid password");
+        }
+
+        user.setIsOnline(true);
 
         userRepository.save(user);
     }
