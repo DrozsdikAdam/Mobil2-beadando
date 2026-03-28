ed29d0e73f80cf11f0cc4f98ba9ac785986c4338
DrozsdikAdam <drozsdik17@gmail.com>
2026-03-28 18:29:51 +0100

    Adding checks for the soft delete attributes

--- RealtimeChatBackend/src/main/java/com/example/realtimechatbackend/repository/UserRepository.java
+++ RealtimeChatBackend/src/main/java/com/example/realtimechatbackend/repository/UserRepository.java
@@ -7,8 +7,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByEmail(String email);
-    Optional<User> findByUsername(String username);
-    Boolean existsByUsername(String username);
-    Boolean existsByEmail(String email);
+    Optional<User> findByEmailAndIsDeletedFalse(String email);
+    Optional<User> findByUsernameAndIsDeletedFalse(String username);
+    Boolean existsByUsernameAndIsDeletedFalse(String username);
+    Boolean existsByEmailAndIsDeletedFalse(String email);
 }
