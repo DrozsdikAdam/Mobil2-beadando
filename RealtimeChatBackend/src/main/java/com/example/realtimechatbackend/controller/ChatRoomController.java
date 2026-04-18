b3c9d07ce1c6fb4c8d467c988b715d7c7b27df23
DrozsdikAdam <drozsdik17@gmail.com>
2026-04-18 19:08:55 +0200

    Created the chatroom controller for the endpoints

---
+++ RealtimeChatBackend/src/main/java/com/example/realtimechatbackend/controller/ChatRoomController.java
@@ -0,0 +1,30 @@
+package com.example.realtimechatbackend.controller;
+
+import com.example.realtimechatbackend.dto.CreateRoomRequestDto;
+import com.example.realtimechatbackend.dto.CreateRoomResponseDto;
+import com.example.realtimechatbackend.model.ChatRoom;
+import com.example.realtimechatbackend.service.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/rooms")
+@RequiredArgsConstructor
+public class ChatRoomController {
+    private final ChatRoomService chatRoomService;
+
+    @PostMapping("/create")
+    public ResponseEntity<CreateRoomResponseDto> createRoom(@RequestBody CreateRoomRequestDto request, Principal principal) {
+        CreateRoomResponseDto response = chatRoomService.createRoom(request, principal.getName());
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<Set<ChatRoom>> getUserRooms(Principal principal) {
+        return ResponseEntity.ok(chatRoomService.getUserRooms(principal.getName()));
+    }
+}
