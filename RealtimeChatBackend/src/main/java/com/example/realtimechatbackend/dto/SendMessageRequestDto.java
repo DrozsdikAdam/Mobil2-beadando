4150e7e103302c93df55f0b8d93db0d073a66978
DrozsdikAdam <drozsdik17@gmail.com>
2026-04-18 17:42:14 +0200

    added the attributes to the send message dto

--- RealtimeChatBackend/src/main/java/com/example/realtimechatbackend/dto/SendMessageRequestDto.java
+++ RealtimeChatBackend/src/main/java/com/example/realtimechatbackend/dto/SendMessageRequestDto.java
@@ -1,4 +1,11 @@
 package com.example.realtimechatbackend.dto;
 
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class SendMessageRequestDto {
+    private String content;
+    private UUID chatRoomId;
 }
