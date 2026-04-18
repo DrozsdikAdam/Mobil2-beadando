95be2027620cbdbdd4f96c6c7755e7320138a031
DrozsdikAdam <drozsdik17@gmail.com>
2026-04-18 17:46:49 +0200

    added the attributes to the create room dto

--- RealtimeChatBackend/src/main/java/com/example/realtimechatbackend/dto/CreateRoomRequestDto.java
+++ RealtimeChatBackend/src/main/java/com/example/realtimechatbackend/dto/CreateRoomRequestDto.java
@@ -1,4 +1,13 @@
 package com.example.realtimechatbackend.dto;
 
+import lombok.Data;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
 public class CreateRoomRequestDto {
+    private String name;
+    private Boolean isGroup;
+    private List<UUID> userIds;
 }
