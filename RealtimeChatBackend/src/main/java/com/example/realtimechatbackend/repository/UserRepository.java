489c889b0c03f59015783098f08c388fb7eafd3b
DrozsdikAdam <drozsdik17@gmail.com>
2026-04-19 11:34:47 +0200

    adding new methods for user repository

--- RealtimeChatBackend/src/main/java/com/example/realtimechatbackend/repository/UserRepository.java
+++ RealtimeChatBackend/src/main/java/com/example/realtimechatbackend/repository/UserRepository.java
@@ -2,7 +2,10 @@ package com.example.realtimechatbackend.repository;
 
 import com.example.realtimechatbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,4 +14,12 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsernameAndIsDeletedFalse(String username);
     Boolean existsByUsernameAndIsDeletedFalse(String username);
     Boolean existsByEmailAndIsDeletedFalse(String email);
+    List<User> findByUsernameContainingIgnoreCaseAndIsDeletedFalse(String username);
+    
+    @Query("SELECT u FROM User u " +
+            "WHERE u.username != :currentUsername AND u.isDeleted = false AND u NOT IN " +
+           "(SELECT u2 FROM ChatRoom " +
+            "c JOIN c.users u1 JOIN c.users u2 " +
+            "WHERE c.isGroup = false AND u1.username = :currentUsername)")
+    List<User> findRecommendedUsers(@Param("currentUsername") String currentUsername);
 }
