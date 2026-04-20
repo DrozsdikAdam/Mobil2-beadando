1ac242b866712bad4bdfd8ae4f68ddcef850fddf
DrozsdikAdam <drozsdik17@gmail.com>
2026-04-20 20:48:58 +0200

    modified the chatmodel

--- RealTimeChatApplication/app/src/main/java/com/example/realtimechatapplication/models/ChatModel.java
+++ RealTimeChatApplication/app/src/main/java/com/example/realtimechatapplication/models/ChatModel.java
@@ -1,18 +1,24 @@
 package com.example.realtimechatapplication.models;
 
+import java.util.UUID;
+
 public class ChatModel {
 
+    private UUID id;
     private String name;
     private String lastMessage;
     private String timeStamp;
     private int profileImage;
 
-    public ChatModel(String name, String lastMessage, String timeStamp, int profileImage) {
+    public ChatModel(UUID id, String name, String lastMessage, String timeStamp, int profileImage) {
+        this.id = id;
         this.name = name;
         this.lastMessage = lastMessage;
         this.timeStamp = timeStamp;
         this.profileImage = profileImage;
     }
+
+    public UUID getId() { return id; }
 
     public String getName() { return name; }
 
