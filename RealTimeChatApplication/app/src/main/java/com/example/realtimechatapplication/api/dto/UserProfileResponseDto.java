69e9ce548a46889570c79152b177f921ab2eca7a
DrozsdikAdam <drozsdik17@gmail.com>
2026-04-20 20:46:14 +0200

    adding new dtos

--- RealTimeChatApplication/app/src/main/java/com/example/realtimechatapplication/api/dto/UserProfileResponseDto.java
+++ RealTimeChatApplication/app/src/main/java/com/example/realtimechatapplication/api/dto/UserProfileResponseDto.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 public class UserProfileResponseDto {
     private UUID id;
     private String username;
+    private String email;
     private Boolean isOnline;
 
     public UUID getId() { return id; }
@@ -12,6 +13,9 @@ public class UserProfileResponseDto {
 
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
     public Boolean getIsOnline() { return isOnline; }
     public void setIsOnline(Boolean online) { isOnline = online; }
---
+++ RealtimeChatBackend/src/main/java/com/example/realtimechatbackend/dto/CurrentUserProfileResponseDto.java
@@ -0,0 +1,20 @@
+package com.example.realtimechatbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class CurrentUserProfileResponseDto {
+    private UUID id;
+    private String username;
+    private String email;
+    private String password;
+    private Boolean isOnline;
+}
