fb11941dd9f994f4d739759097dc5461edd33441
DrozsdikAdam <drozsdik17@gmail.com>
2026-05-01 18:36:24 +0200

    Refactor Created an isSentBy method

--- RealTimeChatApplication/app/src/main/java/com/example/realtimechatapplication/models/MessageModel.java
+++ RealTimeChatApplication/app/src/main/java/com/example/realtimechatapplication/models/MessageModel.java
@@ -1,5 +1,7 @@
 package com.example.realtimechatapplication.models;
 
+import com.example.realtimechatapplication.api.dto.MessageResponseDto;
+
 public class MessageModel {
 
     private String Id;
@@ -49,5 +51,28 @@ public class MessageModel {
 
     public void setIsDeleted(Boolean deleted) {
         isDeleted = deleted;
+    }
+
+    public boolean isSentBy(String currentUserId) {
+        return this.sender != null && this.sender.getUserId() != null && this.sender.getUserId().equals(currentUserId);
+    }
+
+    public static MessageModel fromDto(MessageResponseDto dto, String currentUserId, String currentUsername) {
+        MessageModel model = new MessageModel();
+        model.setId(dto.getId().toString());
+        model.setContent(dto.getContent());
+
+        UserModel sender = new UserModel();
+        sender.setUserName(dto.getSenderUsername());
+
+        if (dto.getSenderUsername().equals(currentUsername)) {
+            sender.setUserId(currentUserId);
+        } else {
+            // Ideiglenes ID a másik felhasználónak, ha nincs jobb megoldás
+            sender.setUserId("other_user_" + dto.getSenderUsername());
+        }
+
+        model.setSender(sender);
+        return model;
     }
 }
