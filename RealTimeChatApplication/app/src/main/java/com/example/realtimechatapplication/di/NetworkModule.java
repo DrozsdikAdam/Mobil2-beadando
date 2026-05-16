80b5e6babbd1fc81fd7bbd68cb1842c7aa7bcd0e
DrozsdikAdam <drozsdik17@gmail.com>
2026-05-16 14:12:32 +0200

    adding network module with static auth token for now

---
+++ RealTimeChatApplication/app/src/main/java/com/example/realtimechatapplication/di/NetworkModule.java
@@ -0,0 +1,61 @@
+package com.example.realtimechatapplication.di;
+
+import com.example.realtimechatapplication.api.ApiService;
+import com.example.realtimechatapplication.api.RetrofitClient;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@Module
+@InstallIn(SingletonComponent.class)
+public class NetworkModule {
+
+    @Singleton
+    @Provides
+    public HttpLoggingInterceptor provideHttpLoggingInterceptor(){
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        return interceptor;
+    }
+    @Singleton
+    @Provides
+    public OkHttpClient provideOkHttpClient(HttpLoggingInterceptor interceptor){
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .addInterceptor(chain -> {
+                    Request original = chain.request();
+                    Request.Builder requestBuilder = original.newBuilder();
+                    requestBuilder.header("Authorization", "Bearer " + RetrofitClient.getAuthToken());
+                    Request request = requestBuilder.build();
+                    return chain.proceed(request);
+                })
+                .build();
+        return client;
+    }
+    @Singleton
+    @Provides
+    public Retrofit provideRetrofit( OkHttpClient client){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:8080/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+        return retrofit;
+    }
+    @Singleton
+    @Provides
+    public ApiService provideApiService(Retrofit client){
+        ApiService service = client.create(ApiService.class);
+        return  service;
+    }
+
+}
