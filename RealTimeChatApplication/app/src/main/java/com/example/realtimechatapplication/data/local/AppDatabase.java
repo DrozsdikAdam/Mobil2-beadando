79e9e5840b0788cec5b500019726fb8263f9b15e
DrozsdikAdam <drozsdik17@gmail.com>
2026-05-16 17:18:05 +0200

    deleting unused methods from appdatabase

--- RealTimeChatApplication/app/src/main/java/com/example/realtimechatapplication/data/local/AppDatabase.java
+++ RealTimeChatApplication/app/src/main/java/com/example/realtimechatapplication/data/local/AppDatabase.java
@@ -1,12 +1,7 @@
 package com.example.realtimechatapplication.data.local;
 
-
-import android.content.Context;
-
 import androidx.room.Database;
-import androidx.room.Room;
 import androidx.room.RoomDatabase;
-import androidx.room.TypeConverter;
 import androidx.room.TypeConverters;
 
 import com.example.realtimechatapplication.data.local.dao.ChatRoomDao;
@@ -14,26 +9,11 @@ import com.example.realtimechatapplication.data.local.dao.MessageDao;
 import com.example.realtimechatapplication.data.local.entity.ChatRoomEntity;
 import com.example.realtimechatapplication.data.local.entity.MessageEntity;
 
-@Database(entities = {ChatRoomEntity.class, MessageEntity.class}, version = 2)
+@Database(entities = {ChatRoomEntity.class, MessageEntity.class}, version = 2, exportSchema = false)
 @TypeConverters({Converter.class})
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract ChatRoomDao chatRoomDao();
     public abstract MessageDao messageDao();
-
-    private static volatile AppDatabase instance;
-
-    public static AppDatabase getDatabase(final Context context){
-        if (instance != null){
-            return instance;
-        }
-        synchronized (AppDatabase.class) {
-            if (instance != null){
-                return instance;
-            }
-            instance = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "chat_database").fallbackToDestructiveMigration().build();
-            return instance;
-        }
-    }
 
 }
